03eec132741dd002b40137e4347591bd68bbde9d
Prateek Kumar <prateekptp@gmail.com>
2024-04-06 11:05:28 +0530

    Setting up seeds directory

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const path = require('path')
 const app = express()
+const CampGround = require('./models/campground')
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "connection error:"));
+db.once('open', () => {
+  console.log("Database connected");
+});
 
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/home', (req,res) => {
+app.get('/', async (req,res) => {
     res.render('home')
+})
+
+app.get('/makecampground', async (req,res) => {
+    const camp = new CampGround({title: 'My Backyard', description: 'cheap camping!'});
+    camp.save();
+    res.send(camp)
 })
 
 app.listen(3000, () => {
