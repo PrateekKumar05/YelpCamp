e921f3faf99761797ed58e29787999bcaebf4da7
Prateek Kumar <prateekptp@gmail.com>
2024-04-21 01:54:14 +0530

    complete authorization

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
-const passport = require('passport');
 const { storeReturnTo } = require('../middleware');
 
-router.get('/register', (req,res) => {
+router.get('/register', (req, res) => {
     res.render('users/register');
-})
+});
 
-router.post('/register', catchAsync(async (req,res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
@@ -25,15 +25,15 @@ router.post('/register', catchAsync(async (req,res) => {
     }
 }));
 
-router.get('/login', (req,res) => {
+router.get('/login', (req, res) => {
     res.render('users/login');
 })
 
-router.post('/login', storeReturnTo, passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req, res) => {
-        req.flash('success', 'Welcome back!');
-        const redirectUrl = res.locals.returnTo || '/campgrounds';
-        res.redirect(redirectUrl);
-    });
+router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome back!');
+    const redirectUrl = res.locals.returnTo || '/campgrounds'; // update this line to use res.locals.returnTo now
+    res.redirect(redirectUrl);
+});
 
 router.get('/logout', (req, res, next) => {
     req.logout(function (err) {
@@ -43,6 +43,6 @@ router.get('/logout', (req, res, next) => {
         req.flash('success', 'Goodbye!');
         res.redirect('/campgrounds');
     });
-}); 
+});
 
 module.exports = router;
