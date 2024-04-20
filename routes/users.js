54533ed01ce85bc0d228910ff394817378276520
Prateek Kumar <prateekptp@gmail.com>
2024-04-21 00:11:12 +0530

    added login routes and middlewares

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,12 @@ const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
+const passport = require('passport');
 
 router.get('/register', (req,res) => {
     res.render('users/register');
 })
+
 router.post('/register', catchAsync(async (req,res) => {
     try {
         const { email, username, password } = req.body;
@@ -18,4 +20,14 @@ router.post('/register', catchAsync(async (req,res) => {
         res.redirect('register');
     }
 }));
+
+router.get('/login', (req,res) => {
+    res.render('users/login');
+})
+
+router.post('/login', passport.authenticate('local', {failureFlash: true, failureRedirect: '/login'}), (req,res) => {
+    req.flash('success', 'Welcome Back!')
+    res.redirect('/campgrounds');
+})
+
 module.exports = router;
